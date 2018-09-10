32122d150804d881d61defeb259e7dc146a82127
Dubidu1212 <rz.ausserschwand@gmail.com>
2018-09-10 15:38:33 +0200

    added countdown and vote functionality

--- code/bot.py
+++ code/bot.py
@@ -2,9 +2,17 @@ import logging
 import discord
 import asyncio
 import secrets
+import vote
 
 logging.basicConfig(level=logging.INFO)
 client = discord.Client()
+finishedSetup = True
+voting = True
+N = 2#number players
+votes = [0 for i in range(N)]
+AlreadyVoted = [False for i in range(N)]
+
+
 
 @client.event
 async def on_ready():
@@ -13,18 +21,26 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+async def countdown(t,times,channel):#t = time // times = repetitions of t // channel = channel on whitch countdown is sent
+    for i in range(times):
+        await asyncio.sleep(t)
+        await client.send_message(channel, str(t*times - i*t) + " seconds left")
+
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
+    if(message.author.id == client.user.id):
+        return
+    if(voting):
+        await countdown(1,5,message.channel)
+        if(( len(message.mentions)==1 ) and (not AlreadyVoted[players[message.author.id]])):
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+            AlreadyVoted[players[message.author.id]] = True
+            votes[players[message.raw_mentions[0]]]+=1
+
+
+    #if message.content.startswith():
+    #    await client.send_message(message.channel,message.channel.name)
+
+
 
 client.run(secrets.username, secrets.password)
