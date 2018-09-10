27e306db8504ad1cb3e4a508ce7fbaa347db7811
Gian-Andrea Cantiello <gian-andrea.cantiello@gmx.ch>
2018-09-10 16:13:39 +0200

    game.py changed dictionary to list

--- code/game.py
+++ code/game.py
@@ -2,6 +2,7 @@ import discord
 import logging
 import secrets
 import random
+import asyncio
 
 logging.basicConfig(level = logging.INFO)
 client = discord.Client()
@@ -10,39 +11,51 @@ server = discord.Server
 started = False
 characters = ["Seer", "Witch", "Cupid", "Hunter", "Midwife", "Priest", "Village bicycle"]
 players = {} #dictionary key: player's id, value: player's number
-playerCharacter = {} #dictionary key: player's number, value: his character
+playerCharacter = [] #dictionary key: player's number, value: his character
 
 @client.event
 async def on_message(message):
+    global channel
+    channel = message.channel
+
     if message.content.lower() == "start" and not started: #if the message is "started and the game isn't
                                                                   # started yet, start it
-        aMembers = server.member_count #number of members
-        members = server.members #all members
-        occupated = [False for a in range(aMembers - 1)] #store the characters which are already taken
+        distributeCharacters()
 
-        if aMembers < 5: #there must be at least 5 players
-            print("Not enough players")
-            return
+def distributeCharacters():
+    started = True
+    aMembers = server.member_count  # number of members
+    members = server.members  # all members
+    occupated = [False for a in range(aMembers - 1)]  # store the characters which are already taken
 
-        aWerewolfs = int(round(aMembers * 0.3, 0)) #number of werewolfs (30%)
-        count = 0
+    if aMembers < 5:  # there must be at least 5 players
+        print("Not enough players")
+        return
 
-        for a in members:
-            players[a.id] = count #assign a number to each player
-            ran = random.randint(0, aMembers - 1) #pick a random role for this player
+    aWerewolfs = int(round(aMembers * 0.3, 0))  # number of werewolfs (30%)
+    count = 0
 
-            while occupated[ran]: #if this roll is already taken, look for another one
-                ran = random.randint(0, aMembers - 1)
+    for a in members:
+        players[a.id] = count  # assign a number to each player
+        ran = random.randint(0, aMembers - 1)  # pick a random role for this player
 
-            occupated[ran] = True
+        while occupated[ran]:  # if this roll is already taken, look for another one
+            ran = random.randint(0, aMembers - 1)
 
-            if ran >= 6 + aWerewolfs:
-                playerCharacter[count] = "Villager"
-            elif ran <= aWerewolfs:
-                playerCharacter[count] = "Werewolf"
-            else:
-                playerCharacter[count] = characters[ran]
+        occupated[ran] = True
 
-            count += 1
+        if ran >= 6 + aWerewolfs:
+            list(playerCharacter[8]).append(count)
+        elif ran <= aWerewolfs:
+            (playerCharacter[0]).append(count)
+        else:
+            playerCharacter[characters[ran]] = ran
+
+        count += 1
+
+def story():
+    client.send_message(channel, "It turns night in our small village and all the villagers got to bed")
+    asyncio.sleep(3)
+    client.send_message(channel, "Cupid, wake up, it's time for love!")
 
 client.run(secrets.username, secrets.password)
