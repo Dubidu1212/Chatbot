9acce28c50632698ee63356c601431097591d6c5
Gian-Andrea Cantiello <gian-andrea.cantiello@gmx.ch>
2018-09-10 10:11:53 +0200

    modified bot.py

--- code/bot.py
+++ code/bot.py
@@ -1,9 +1,29 @@
 import logging
 import discord
+import asyncio
 
 logging.basicConfig(level=logging.INFO)
+client = discord.Client()
 
-def main():
-    pass
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
 
-main()
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run('token')
