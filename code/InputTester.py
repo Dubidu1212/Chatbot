15cd7ff2d6cf928e388415eb5346fa3b790fc522
Dubidu1212 <rz.ausserschwand@gmail.com>
2018-09-12 09:14:43 +0200

    added controll functions

---
+++ code/InputTester.py
@@ -0,0 +1,20 @@
+def testNumberpair(in,max):#in: string // max = num Player
+    a = in.split(" ")
+    if(len(a) != 2):
+        return False
+    try:
+        b = list(map(int,a))
+    except ValueError:
+        return False
+    if b[0]>0 and b[0]<=max:
+        if b[1]>0 and b[1]<=max:
+            return True
+    return False
+def testNumberOne(in):
+    try:
+        a = int(in)
+        if(a <= max and a > 0):
+            return True
+        return False
+    except ValueError:
+        return False
