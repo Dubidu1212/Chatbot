adfe3234a5d09505ee1112f7427708c4ca29d375
Dubidu1212 <rz.ausserschwand@gmail.com>
2018-09-12 11:53:11 +0200

    finished witch

--- code/witch.py
+++ code/witch.py
@@ -1,3 +1,21 @@
+
+WitchKilling #bool
+WitchKillingInfoSent #bool
+
+
+elif WitchActive and not WitchKillingInfoSent:
+    await client.send_message(memberDict[numToId[playerCharacter[3][0]]], "Type the number of the one to kill")
+    tempMessage = []
+    for i in range(N):
+        tempMessage.append(str(i+1) + ": " +memberDict[numToId[i]].name + "\n")
+    s = ''.join(tempMessage)
+    await client.send_message(memberDict[numToId[playerCharacter[3][0]]],s)
+    WitchKillingInfoSend = True
+
+
+
+
+
 elif WitchActive:
     if message.author.id == numToId[playerCharacter[3][0]]:
         d = message.content.find(":-1:")#thumbsdown
@@ -9,7 +27,7 @@ elif WitchActive:
                     await client.send_message(message.channel, "Are you trying to kid me?? You have to answer with :-1:,:+1: or :fist:!")
                 else:
                     WitchActive = False
-                    await client.send_message(message.cannel, "YOu don't feel pity for this poor dude? You're such a witch!")
+                    await client.send_message(message.cannel, "You don't feel pity for this poor dude? You're such a witch!")
             else:
                 await client.send_message(message.channel, "Ooh you're healing, that's very kind! Maybe this this dude is your secret love?:smirk:")
                 # TODO: remove the person from deathList if is in love remove love too
@@ -17,4 +35,9 @@ elif WitchActive:
         else:#thumb is down
             await client.send_message(message.channel,"Feeling like killing someone, are you?:skull:")
             # TODO: doing killing stuff
+            WitchKilling = True
             WitchActive = False
+elif WitchKilling:
+    if messages.author.id == numToId[playerCharacter[3][0]]:
+        kill(int(message.content)-1,True)
+        await client.send_message(message.channel,"The job is done")
