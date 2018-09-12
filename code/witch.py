4c5f92d5a4c92204abaddf8b9e34b632ea74d59b
Gian-Andrea Cantiello <gian-andrea.cantiello@gmx.ch>
2018-09-12 11:22:02 +0200

    continued WitchActive

--- code/witch.py
+++ code/witch.py
@@ -6,15 +6,15 @@ elif WitchActive:
             if u == -1:
                 n = message.content.find(":fist:")#fist
                 if n == -1:
-                    await client.send_message(message.channel,"You have to answer with :-1:,:+1: or :fist:")
+                    await client.send_message(message.channel, "Are you trying to kid me?? You have to answer with :-1:,:+1: or :fist:!")
                 else:
                     WitchActive = False
-                    return
+                    await client.send_message(message.cannel, "YOu don't feel pity for this poor dude? You're such a witch!")
             else:
-                await client.send_message(message.channel,"some healing stuff")
+                await client.send_message(message.channel, "Ooh you're healing, that's very kind! Maybe this this dude is your secret love?:smirk:")
                 # TODO: remove the person from deathList if is in love remove love too
                 WitchActive = False
         else:#thumb is down
-            await client.send_message(message.channel,"Feeling like killing someone are you? :skull: ")
+            await client.send_message(message.channel,"Feeling like killing someone, are you?:skull:")
             # TODO: doing killing stuff
             WitchActive = False
