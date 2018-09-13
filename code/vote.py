01f4dd470ba5bf7e1d8ba22b2b2b8d557003a70c
Gian-Andrea Cantiello <gian-andrea.cantiello@gmx.ch>
2018-09-13 14:33:48 +0200

    added vote story

--- code/vote.py
+++ code/vote.py
@@ -12,3 +12,6 @@ async def on_message(message):
     print(len(message.mentions))
 
 client.run(secrets.username, secrets.password)
+
+await client.send_message(channel, "I know some of you are suspecting certain villagers to be a werewolf. Now you have the opportunity the vote for someone whom you want to kill. But first, convince the others of your suspicion!")
+await client.send_message(channel, "30 seconds to vote!")
